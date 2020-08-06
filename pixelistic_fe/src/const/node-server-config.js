7402fe1544ac5f16cc1f5d3cdfc35d09d835c252
Yaroslav Gatalyak <gatalyak@yahoo.com>
2020-08-06 11:16:37 -0400

    implemented env

--- pixelistic_fe/src/const/node-server-config.js
+++ pixelistic_fe/src/const/node-server-config.js
@@ -1,3 +1,3 @@
 export const port = 3000;
-export const host = 'http://10.26.1.171';
+export const host = process.env.REACT_APP_API_WEB;
 export const awsImage = 'https://s3.us-east-2.amazonaws.com/pixelsss';
