00e7efa09454de63f6d296e37e74e9e041626c56
Yaroslav Gatalyak <gatalyak@gmail.com>
2020-09-17 13:36:36 +0300

    fix env

--- pixelistic_fe/src/const/node-server-config.js
+++ pixelistic_fe/src/const/node-server-config.js
@@ -1,4 +1,6 @@
 export const port = 3000;
 //export const host = process.env.REACT_APP_API_WEB;
-export const host = window._env_.API_URL
-export const awsImage = process.env.REACT_APP_AWS_S3;
+//export const awsImage = process.env.REACT_APP_AWS_S3;
+
+export const host = window._env_.REACT_APP_API_WEB;
+export const awsImage = window._env_.REACT_APP_AWS_S3;
