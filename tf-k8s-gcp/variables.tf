a131acae67eaeec6de17b3a1ecec85d74d494988
Yaroslav Gatalyak <gatalyak@yahoo.com>
2020-09-28 20:19:32 +0300

    fix tf

--- tf-k8s-gcp/variables.tf
+++ tf-k8s-gcp/variables.tf
@@ -17,13 +17,6 @@ variable "database_name" {
   description = "The database name for Production"
 }
 
-variable "database_username" {
-  description = "The username for the Production database"
-}
-
-variable "database_password" {
-  description = "The user password for the Production database"
-}
 
 variable "environment" {
 	description = "Environment for the application"
@@ -32,22 +25,5 @@ variable "environment" {
 variable "availability_zones" {
 	type = list
 }
-
-variable "AWS_ACCESS_KEY_ID" {
-  description = "The AWS_ACCESS_KEY_ID"
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "The AWS_SECRET_ACCESS_KEY"
-}
-
-variable "EMAIL_USER" {
-  description = "The EMAIL_USER"
-}
-
-variable "EMAIL_PASS" {
-  description = "The EMAIL_PASS"
-}
-
 
 
